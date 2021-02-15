6ed82e74cb4a6e49c65db894a424cf2ea0bf6c19
Christof <bauer.christof@gmail.com>
2021-02-15 01:10:52 -0500

    Add labels to Textareas

--- components/form/TextArea.js
+++ components/form/TextArea.js
@@ -1,7 +1,9 @@
-const TextArea = ({ value, name, placeholder, onChange, required }) => {
+const TextArea = ({ value, name, label, placeholder, onChange, required }) => {
 
   return (
-    <label htmlFor={name} className="w-full mb-8">
+    <label htmlFor={name} className="overlap mb-8 relative w-full cursor-pointer">
+      <span className="absolute bg-white text-gray-500 text-xs left-6 -top-2 px-2">{label}</span>
+
       <textarea
         name={name}
         id={name}
@@ -10,8 +12,7 @@ const TextArea = ({ value, name, placeholder, onChange, required }) => {
         placeholder={placeholder}
         required={required}
         className="w-full border border-gray-300 rounded shadow-md px-6 py-4 h-64 focus:shadow-inner"
-      >
-      </textarea>
+      />
     </label>
   )
 }
