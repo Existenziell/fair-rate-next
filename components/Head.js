371f65b9504452915a0600928fe590b05fa012bd
Christof <bauer.christof@gmail.com>
2021-02-21 20:59:59 -0500

    iOS specifics

--- components/Head.js
+++ components/Head.js
@@ -2,7 +2,7 @@ import NextHead from 'next/head'
 
 const Head = () => (
   <NextHead>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no' />
     <meta name='description' content='Stem cell treatment clinic located in Cozumel, Mexico. We follow the highest standard, respecting safety and innovation in the delivery of regenerative medicine' />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merriweather:wght@100;200;300;400;500;600;700;800;900&display=swap" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@100;200;300;400;500;600;700;800;900&display=swap" />
